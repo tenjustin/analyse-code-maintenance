cc8e2c077aa1a472c464db72b09c8a4691e0851f
Justin TENE <tene.justin@gmail.com>
2025-06-10 15:40:30 +0200

    feat(users): swagger, db and CRUD

--- mediatheque-app/src/main/java/org/example/mediatheque/Models/User.java
+++ mediatheque-app/src/main/java/org/example/mediatheque/Models/User.java
@@ -1,4 +1,24 @@
 package org.example.mediatheque.Models;
 
+import jakarta.persistence.*;
+
 @Entity
-public record User(String id, String username, String email, String password) {}
+@Table(name = "users")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+    public String username;
+    public String email;
+    public String password;
+
+    public User() {
+    }
+
+    public User(int id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
